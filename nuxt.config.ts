1665df858140170b53376dea7b319dc75bc81cb2
Ryoh Hashimoto <hashimotoryoh@gmail.com>
2026-07-22 18:29:50 +0900

    `npm run format`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,17 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: [
-    '@nuxt/eslint',
-  ],
+  modules: ['@nuxt/eslint'],
   typescript: {
     strict: true,
   },
   app: {
     head: {
-      meta: [
-        { name: 'x-app-version', content: process.env.APP_VERSION },
-      ],
+      meta: [{ name: 'x-app-version', content: process.env.APP_VERSION }],
     },
   },
 })
